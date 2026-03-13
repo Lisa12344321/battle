d45f743d010cc90ab3e745627535be110f3b4ffe
Lisa12344321 <lisa.alfven@elev.ga.ntig.se>
2026-03-13 09:39:26 +0100

    hej

--- app.py
+++ app.py
@@ -1,39 +1,75 @@
 import random
 
-attacker = ["Slag", "Spark", "Eldklot", "Heal"]
+#attacker = ["Slag", "Spark", "Eldklot", "Heal"]
+attacker = [
+    {
+        "name": "Slag",
+        "skada": 5,
+    },
+    {
+        "name": "Spark",
+        "skada": 10,
+    },
+    {
+        "name": "Eldklot",
+        "skada": 20, 
+    },
+    {
+        "name": "Heal",
+        "heal": 10,
+    }
+]
+monster_attacker = [
+    {
+        "name": "Slag",
+        "skada": 5, #monstrets skada på spelaren
+    },
+    {
+        "name": "Spark",
+        "skada": 10, #monstrets skada på spelaren
+    },
+    {
+        "name": "Eldklot",
+        "skada": 20, #monstrets skada på spelaren
+    },
+]
 skador = [5, 10, 20]
-heal = 10
 
 hp_spelare = 100
 hp_monster = 100
 
 print("--- FIGHT START ---")
 
-while hp_spelare > 0 and hp_monster > 0:
-    val_attack = input(f"Välg attack [ {" | ".join(attacker)} ]: ").lower()
-    val = random.choice([0, 1, 2])
+print_list = []
+for attack in attacker:
+    print_list.append(attack["name"]) 
 
-    if val_attack.capitalize() not in attacker:
+heal_index = print_list.index("Heal")
+
+while hp_spelare > 0 and hp_monster > 0:
+    
+    val_attack = input(f"Välg attack [ {" | ".join(print_list)} ]: ").lower()
+    skada = random.choice(skador)
+
+    if val_attack.capitalize() not in print_list:
         print("Välj någon av attackerna!\n")
         continue
 
     if val_attack.lower() != "heal":
-        skada = skador[val]
         hp_monster -= skada
-        print(f"Du använder {val_attack.capitalize()}! Monstern tar {skada} skada.")
+        print(f"Du använder {val_attack.capitalize()}! Monstret tar {skada} skada.")
     else:
-        hp_spelare += heal
-        print(f"Du använder {val_attack.capitalize()}! Du healas {heal} hp.")
+        hp_spelare += attacker[heal_index]["heal"]
+        print(f"Du använder {val_attack.capitalize()}! Du healas {attacker[heal_index]["heal"]} hp.")
 
     if hp_monster > 0:
-        val = random.choice([0, 1, 2])
-        skada = skador[val]
-        hp_spelare -= skada
-        print(f"Monstret använder {attacker[val]}! Du tar {skada} skada.")
+       monster_attack = random.choice(monster_attacker)
+       hp_spelare -= monster_attack["skada"]
+       print(f"Monstret använder {monster_attack["name"]}! Du tar {monster_attack["skada"]} skada.")
     
-    print(f"Din HP: {hp_spelare} | Monster HP: {hp_monster}\n")
+#     #print(f"Din HP: {hp_spelare} | Monster HP: {hp_monster}\n")
 
-if hp_spelare > 0:
-    print("Du vann!")
-else:
-    print("Du dog...")
+# if hp_spelare > 0:
+#     print("Du vann!")
+# else:
+#     print("Du dog...")
